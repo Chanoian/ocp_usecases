db2070ca304ace698d52dcf86df005689ff62872
Ara X. Chanoian <Ara.X.Chanoian@kp.org>
2023-10-03 16:15:05 -0400

    add cronJob use case

--- cron_jobs/app.py
+++ cron_jobs/app.py
@@ -1,11 +1,12 @@
 import requests
+import logging
 
 def main():
     response = requests.get('https://google.com')
     if response.status_code == 200:
-        print ('Request Was Successful !!')
+        logging.debug('Request Was Successful !!')
     else:
-        print(f'Error: {response.status_code}')
+        logging.debug(f'Error: {response.status_code}')
 
 if __name__ == '__main__':
     main()
--- testing_emptydir_full/README.md
+++ testing_emptydir_full/README.md
@@ -1,6 +1,7 @@
 # testing_emptydir_full
 
-This will test what will happens when the emptyDir gets over sizeLimit
+This will test what will happens when the emptyDir gets over sizeLimit using 
+dd if=/dev/urandom of=test1 bs=1M count=10240
 
 # Findings
 
